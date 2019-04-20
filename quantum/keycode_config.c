a7600e401a9e032dafa5954827ad11f84a578c91
Tetsuya Shinone <info@semnil.com>
2019-04-20 15:45:09 +0900

    Disable swap key of GUI and ALT

--- quantum/keycode_config.c
+++ quantum/keycode_config.c
@@ -43,23 +43,23 @@ uint16_t keycode_config(uint16_t keycode) {
             }
             return KC_LCTL;
         case KC_LALT:
-            if (keymap_config.swap_lalt_lgui) {
-                if (keymap_config.no_gui) {
-                    return KC_NO;
-                }
-                return KC_LGUI;
-            }
+//            if (keymap_config.swap_lalt_lgui) {
+//                if (keymap_config.no_gui) {
+//                    return KC_NO;
+//                }
+//                return KC_LGUI;
+//            }
             return KC_LALT;
         case KC_LGUI:
-            if (keymap_config.swap_lalt_lgui) {
-                return KC_LALT;
-            }
-            if (keymap_config.swap_lctl_lgui) {
-                return KC_LCTRL;
-            }
-            if (keymap_config.no_gui) {
-                return KC_NO;
-            }
+            // if (keymap_config.swap_lalt_lgui) {
+            //     return KC_LALT;
+            // }
+            // if (keymap_config.swap_lctl_lgui) {
+            //     return KC_LCTRL;
+            // }
+            // if (keymap_config.no_gui) {
+            //     return KC_NO;
+            // }
             return KC_LGUI;
         case KC_RCTL:
             if (keymap_config.swap_rctl_rgui) {
@@ -70,23 +70,23 @@ uint16_t keycode_config(uint16_t keycode) {
             }
             return KC_RCTL;
         case KC_RALT:
-            if (keymap_config.swap_ralt_rgui) {
-                if (keymap_config.no_gui) {
-                    return KC_NO;
-                }
-                return KC_RGUI;
-            }
+//            if (keymap_config.swap_ralt_rgui) {
+//                if (keymap_config.no_gui) {
+//                    return KC_NO;
+//                }
+//                return KC_RGUI;
+//            }
             return KC_RALT;
         case KC_RGUI:
-            if (keymap_config.swap_ralt_rgui) {
-                return KC_RALT;
-            }
-            if (keymap_config.swap_rctl_rgui) {
-                return KC_RCTL;
-            }
-            if (keymap_config.no_gui) {
-                return KC_NO;
-            }
+            // if (keymap_config.swap_ralt_rgui) {
+            //     return KC_RALT;
+            // }
+            // if (keymap_config.swap_rctl_rgui) {
+            //     return KC_RCTL;
+            // }
+            // if (keymap_config.no_gui) {
+            //     return KC_NO;
+            // }
             return KC_RGUI;
         case KC_GRAVE:
             if (keymap_config.swap_grave_esc) {
